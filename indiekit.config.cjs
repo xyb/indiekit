f4b6e57d21c2ede1cd7263627a751cfbf94c2ad0
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-11-08 16:18:27 +0000

    feat(endpoint-activitypub): wip

--- indiekit.config.cjs
+++ indiekit.config.cjs
@@ -10,6 +10,7 @@ module.exports = {
     }),
   },
   plugins: [
+    "@indiekit/endpoint-activitypub",
     "@indiekit/preset-jekyll",
     "@indiekit/store-file-system",
     "@indiekit/syndicator-mastodon",
@@ -18,6 +19,21 @@ module.exports = {
   publication: {
     me: process.env.PUBLICATION_URL,
     timeZone: process.env.TZ,
+  },
+  "@indiekit/endpoint-activitypub": {
+    ...(process.env.RAILWAY_ENVIRONMENT && {
+      domain: process.env.RAILWAY_STATIC_URL,
+    }),
+    username: "indiekit",
+    displayName: "Indiekit",
+    summary: "Testing Indiekit’s ActivityPub integration",
+    icon: {
+      type: "Image",
+      mediaType: "image/png",
+      url: "https://avatars.githubusercontent.com/u/68553280?s=180",
+      width: 180,
+      height: 180,
+    },
   },
   "@indiekit/store-file-system": {
     directory: "tmp",

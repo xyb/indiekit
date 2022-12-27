6f9b6238dc9b39a5499fa1f39bd63380d6cdb889
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2022-12-27 20:55:14 +0000

    feat(endpoint-micropub): use cursor pagination

--- packages/endpoint-micropub/lib/controllers/query.js
+++ packages/endpoint-micropub/lib/controllers/query.js
@@ -1,6 +1,7 @@
 import { IndiekitError } from "@indiekit/error";
 import { getConfig, queryConfig } from "../config.js";
 import { getMf2Properties, jf2ToMf2 } from "../mf2.js";
+import { getCursor } from "../pagination.js";
 
 /**
  * Query previously published posts
@@ -12,6 +13,7 @@ import { getMf2Properties, jf2ToMf2 } from "../mf2.js";
  */
 export const queryController = async (request, response, next) => {
   const { application, publication } = request.app.locals;
+  let { q, url } = request.query;
 
   try {
     if (!application.hasDatabase) {
@@ -22,19 +24,6 @@ export const queryController = async (request, response, next) => {
 
     const config = getConfig(application, publication);
 
-    let { page, limit, offset } = request.query;
-    page = Number.parseInt(page, 10) || 1;
-    limit = Number.parseInt(limit, 10) || 40;
-    offset = Number.parseInt(offset, 10) || (page - 1) * limit;
-
-    const posts = await publication.posts
-      .find()
-      .sort({ "properties.published": -1 })
-      .skip(offset)
-      .limit(limit)
-      .toArray();
-
-    let { filter, properties, q, url } = request.query;
     if (!q) {
       throw IndiekitError.badRequest(
         response.__("BadRequestError.missingParameter", "q")
@@ -64,19 +53,24 @@ export const queryController = async (request, response, next) => {
         // Return mf2 for a given source URL (optionally filtered by properties)
         if (url) {
           const mf2 = jf2ToMf2(item.properties);
+          const { properties } = request.query;
           return response.json(getMf2Properties(mf2, properties));
         }
 
-        // Return mf2 for previously published posts
+        // Return mf2 for previously published posts (cursor paginated)
+        const cursor = await getCursor(publication.posts, request.query);
+
         return response.json({
-          _count: await publication.posts.countDocuments(),
-          items: posts.map((post) => jf2ToMf2(post.properties)),
+          ...(cursor.hasNext && { after: cursor.lastItem }),
+          ...(cursor.hasPrev && { before: cursor.firstItem }),
+          items: cursor.items.map((post) => jf2ToMf2(post.properties)),
         });
       }
 
       default: {
         // Query configuration value (can be filtered, limited and offset)
         if (config[q]) {
+          const { filter, limit, offset } = request.query;
           return response.json({
             [q]: queryConfig(config[q], { filter, limit, offset }),
           });
